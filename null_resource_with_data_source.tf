a5db9c1db335d9a43d8841cd7169ca790e067ebb
Roger Berlind <roger@hashicorp.com>
2017-11-20 18:34:47 -0500

    just use README.md

--- null_resource_with_data_source.tf
+++ null_resource_with_data_source.tf
@@ -7,17 +7,9 @@ variable "name" {
   default = "Roger"
 }
 
-variable "filename" {
-  description = "file name"
-  default = "README.md"
-}
-
 resource "null_resource" "write_file" {
   provisioner "local-exec" {
-    command = "cp ${var.filename} README.txt"
-  }
-  provisioner "local-exec" {
-    command = "echo ${var.name} > ${var.filename}"
+    command = "echo ${var.name} > README.md"
   }
   triggers {
     name = "${var.name}"
@@ -26,19 +18,9 @@ resource "null_resource" "write_file" {
 
 data "null_data_source" "read_file" {
   inputs = {
-    name = "${file("${var.filename}")}"
+    name = "${file("README.md")}"
   }
   depends_on = ["null_resource.write_file"]
-}
-
-resource "null_resource" "restore_sudoers" {
-  provisioner "local-exec" {
-    command = "cp README.txt README.md"
-  }
-  # This is just to make sure that restore_sudoers is done after read_file
-  provisioner "local-exec" {
-    command = "echo ${data.null_data_source.read_file.outputs["name"]}"
-  }
 }
 
 output "name" {
